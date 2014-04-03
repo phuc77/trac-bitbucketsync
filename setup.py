c2175d068372fcd6ee03522b52ac7139bc3dc03f
Lele Gaifax <lele@metapensiero.it>
2014-04-03 12:14:56 +0200

    Bump version to 0.2 and add myself as the maintainer

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup
 
-VERSION = '0.1.1'
+VERSION = '0.2'
 PACKAGE = 'bitbucketsync'
 
-setup(
-	name = 'BitbucketSyncPlugin',
-	version = VERSION,
-	description = "Sync Bitbucket repository with local repository used by Trac.",
-	author = 'Mitar',
-	author_email = 'mitar.trac@tnode.com',
-	url = 'http://mitar.tnode.com/',
-	keywords = 'trac plugin',
-	license = "AGPLv3",
-	packages = [PACKAGE],
-	include_package_data = True,
-	install_requires = [],
-	zip_safe = False,
-	entry_points = {
-		'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE),
-	},
+setup(name = 'BitbucketSyncPlugin',
+      version = VERSION,
+      description = "Sync Bitbucket repository with local repository used by Trac.",
+      author = 'Mitar',
+      author_email = 'mitar.trac@tnode.com',
+      maintainer = 'Lele Gaifax',
+      maintainer_email = 'lele@metapensiero.it',
+      url = 'http://mitar.tnode.com/',
+      keywords = 'trac plugin',
+      license = "AGPLv3",
+      packages = [PACKAGE],
+      include_package_data = True,
+      install_requires = [],
+      zip_safe = False,
+      entry_points = {
+          'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE),
+      },
 )
